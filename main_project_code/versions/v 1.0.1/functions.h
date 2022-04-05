bb9c64217dada62f0a0f0c4c19921a1a35cea9f0
jackl39 <89060650+jackl39@users.noreply.github.com>
2022-04-05 15:35:10 +1000

    Update functions.h

--- main_project_code/versions/v 1.0.1/functions.h
+++ main_project_code/versions/v 1.0.1/functions.h
@@ -1,13 +1,24 @@
 #ifndef FUNCTIONS_HEADER
 #define FUNCTIONS_HEADER
 
+#define SERIAL_BUFFER 30
+
+static int readCounter = 0; // counter to use for reading in data from serial
+static int writeCounter = 0; // counter to use for writing in data from serial
+static int READ_WRITE = 0; // constant used to determine whether the port will be reading/writing
+
+
+static char rawData[SERIAL_BUFFER]; // list to store characters which are read/sent
 
 // function to intialise serial port
 
-void initaliseSerialPorts (void);
+void initialiseSerialPorts (void);
 
 // function to send "Initialisation Successful" to terminal via serial
 
-void displaySuccessfulInit(int readData[]);
+void displaySuccessfulInit(int data[]);
+
+// function to convert the global array from serial.h into a non-global
+// array which can be modified and used in main.c
 
 #endif
