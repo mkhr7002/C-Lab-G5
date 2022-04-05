a77ce9ac05b86f3efdda37336717797166cc34c4
jackl39 <89060650+jackl39@users.noreply.github.com>
2022-04-05 16:57:51 +1000

    Update functions.h

--- main_project_code/versions/v 1.0.1/functions.h
+++ main_project_code/versions/v 1.0.1/functions.h
@@ -6,6 +6,7 @@
 static int readCounter = 0; // counter to use for reading in data from serial
 static int writeCounter = 0; // counter to use for writing in data from serial
 static int READ_WRITE = 0; // constant used to determine whether the port will be reading/writing
+static int carriageFlag = 0; // constant used to determine end of string
 
 
 static char rawData[SERIAL_BUFFER]; // list to store characters which are read/sent
@@ -20,5 +21,13 @@ void displaySuccessfulInit(int data[]);
 
 // function to convert the global array from serial.h into a non-global
 // array which can be modified and used in main.c
+
+// function to read serial data and store in array
+void readSerial(int port);
+
+// reset everything so new input can be received
+void resetNewInput(void);
+
+void processSerialInput(void);
 
 #endif
