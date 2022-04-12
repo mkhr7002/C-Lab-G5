521e08b17440749397f2ce58e20db209a3633f30
jackl39 <89060650+jackl39@users.noreply.github.com>
2022-04-12 15:10:14 +1000

    Update main.c

--- main_project_code/versions/v 2.0.1/main.c
+++ main_project_code/versions/v 2.0.1/main.c
@@ -6,6 +6,15 @@
 serialPort SCI0 = {&SCI0BDH, &SCI0BDL, &SCI0CR1, &SCI0CR2, &SCI0DRL, &SCI0SR1};
 serialPort SCI1 = {&SCI1BDH, &SCI1BDL, &SCI1CR1, &SCI1CR2, &SCI1DRL, &SCI1SR1};
 
+int readCounter; // counter to use for reading in data from serial
+int writeCounter; // counter to use for writing in data from serial
+int READ_WRITE; // constant used to determine whether the port will be reading/writing
+int carriageFlag; // constant used to determine end of string
+
+char* writePointer;
+
+char rawData[SERIAL_BUFFER]; // list to store characters which are read/sent
+
 void main(void) {
   
   char* initString;
@@ -18,7 +27,7 @@ void main(void) {
   // call function to send "Initalisation Successful"
   // to the terminal via the serial port
   
-  //writeStringToSCI(&SCI1, initString);
+  writeStringToSCI(&SCI1, initString);
   DisableInterrupts
   // re-initalise ports for serial reading interrupts
   // initialiseSerialPort(&SCI0);
